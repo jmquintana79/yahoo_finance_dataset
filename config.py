a9b31980aed7c20d52033e2aa5e71318a041ea5d
Juan Quintana <juanq79@pm.me>
2023-08-26 23:22:10 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+# folders
+folder_raw = "data/raw"
+folder_external = "data/external"
+folder_interim = "data/interim"
+folder_processed = "data/processed"
+
+# file
+file_stock_screener = "stock_screener_by_nasdaq.csv"
